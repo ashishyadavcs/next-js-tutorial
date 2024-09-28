34cbcc64e4f724f634b28de32003b48d730910f6
Ashish Kumar <ashishbhu221306@gmail.com>
2024-09-28 12:05:03 +0530

    create layout

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return <StyledHeader>Header</StyledHeader>;
+};
+
+export default Header;
+const StyledHeader = styled.header`
+  color: red;
+  background: red;
+  padding: 20px;
+`;
