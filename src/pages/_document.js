cceba2be4d383f8b129d5a9e3b68e5bc1c2ed952
Ashish Kumar <ashishbhu221306@gmail.com>
2024-11-23 20:16:16 +0530

    servre stylesheet fixed for styled component

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,44 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
 
-export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+    render() {
+        return (
+            <Html lang="en">
+                <Head />
+                <body className="body">
+                    <StyleSheetManager disableCSSOMInjection={false}>
+                        <Main />
+                    </StyleSheetManager>
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
+
+export default MyDocument;
